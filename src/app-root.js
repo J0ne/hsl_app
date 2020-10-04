895c51920b99d25ee3cf7a29258915725b77fb52
Jouni Männistö <jouni.mannisto@gmail.com>
2020-10-04 20:50:51 +0300

    Building map

--- src/app-root.js
+++ src/app-root.js
@@ -29,9 +29,27 @@ export class HslApp extends LitElement {
     return html`
       <div class="wrapper">
         <h1>HSL app</h1>
-          ${this.message}
-        </a>
+          ${this.message} 
       </div>
+      <div id="hsl-map"><</div>
     `;
+  }
+
+  firstUpdated() {
+    const map = L.map('map').setView([60.192059, 24.945831], 15);
+
+    var normalTiles = L.tileLayer(
+      'https://cdn.digitransit.fi/map/v1/{id}/{z}/{x}/{y}.png',
+      {
+        attribution:
+          'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        maxZoom: 19,
+        tileSize: 512,
+        zoomOffset: -1,
+        id: 'hsl-map',
+      },
+    ).addTo(map);
+                
   }
 }
