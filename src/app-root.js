69a0f81e759977f193b60a528fbccf309d27d35f
Jouni Männistö <jouni.mannisto@gmail.com>
2020-10-11 20:50:56 +0300

    Update app-root.js
     Usage of material drawer

--- src/app-root.js
+++ src/app-root.js
@@ -1,11 +1,23 @@
-import { customElement, property, LitElement, html, css } from 'lit-element';
-
-@customElement('app-root')
+import { customElement, property, LitElement, html, css } from "lit-element";
+import "@material/mwc-drawer";
+import "@material/mwc-icon-button";
+import "@material/mwc-top-app-bar";
+@customElement("app-root")
 export class AppRoot extends LitElement {
-  @property() message = 'Learn LitElement';
-
   static get styles() {
     return css`
+      body {
+        height: 100vh;
+      }
+
+      .drawer-content {
+        padding: 0px 16px 0 16px;
+      }
+
+      .main-content {
+        min-height: 300px;
+        padding: 48px 18px 0 18px;
+      }
       h1 {
         font-size: 4rem;
       }
@@ -27,7 +39,76 @@ export class AppRoot extends LitElement {
 
   render() {
     return html`
-      <div class="wrapper"></div>
+      <mwc-drawer hasHeader type="modal">
+        <span slot="title">Drawer Title</span>
+        <span slot="subtitle">subtitle</span>
+        <div class="drawer-content">
+          <p>Drawer content</p>
+          <mwc-icon-button icon="gesture"></mwc-icon-button>
+          <mwc-icon-button icon="gavel" id="gavel"></mwc-icon-button>
+        </div>
+        <div slot="appContent">
+          <mwc-top-app-bar>
+            <mwc-icon-button
+              slot="navigationIcon"
+              icon="menu"
+            ></mwc-icon-button>
+            <div slot="title">HSL App</div>
+            <mwc-icon-button slot="actionItems" icon="cast"></mwc-icon-button>
+            <mwc-icon-button
+              slot="actionItems"
+              icon="fingerprint"
+            ></mwc-icon-button>
+          </mwc-top-app-bar>
+          <div class="main-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </mwc-drawer>
     `;
+  }
+  firstUpdated() {
+    const drawer = this.renderRoot.querySelectorAll("mwc-drawer")[0];
+    if (drawer) {
+      const container = drawer.parentNode;
+      container.addEventListener("MDCTopAppBar:nav", () => {
+        drawer.open = !drawer.open;
+      });
+    }
   }
 }
