8e6f525590dd0866081aa4f8aa0bb104544eca74
Jouni Männistö <jouni.mannisto@gmail.com>
2020-10-25 19:19:11 +0200

    Vehicle selector update

--- src/vehicle-selector.js
+++ src/vehicle-selector.js
@@ -2,11 +2,13 @@ import { LitElement, html, css } from "lit-element";
 import "@material/mwc-select";
 import "@material/mwc-list/mwc-list-item";
 import "@material/mwc-button";
+import "@material/mwc-icon-button-toggle";
 
 class VehicleSelector extends LitElement {
   static get properties() {
     return {
       vehicles: { type: Array },
+      selected: { type: String },
       handleCallback: { type: Object },
     };
   }
@@ -14,16 +16,67 @@ class VehicleSelector extends LitElement {
   constructor() {
     super();
     this.vehicles = [];
+    this.selected = { icon: "check", type: "Select vehicle type" };
   }
-
+  static get styles() {
+    return css`
+      .selected-text {
+        font-weight: 600;
+        text-transform: uppercase;
+      }
+      .selected {
+        font-size: 16px;
+      }
+      .color-size {
+        color: green;
+        --mdc-icon-size: 4em;
+      }
+    `;
+  }
   async connectedCallback() {
     super.connectedCallback();
     this.vehicles = await this.loadVehicles();
+    // alustus
+    this.vehicles.forEach((v) => (v.on = false));
   }
-
+  handleIconBtnToggle(e) {
+    const vehicle = e.currentTarget;
+    this.selected = { type: vehicle.name, icon: vehicle.offIcon };
+    this.vehicles.forEach((v) => {
+      if (v.type !== vehicle.name) {
+        v.on = false;
+      } else {
+        v.on = true;
+      }
+    });
+    this.requestUpdate();
+    this.handleCallback(vehicle.type);
+  }
   render() {
     return html`
-      <mwc-select
+      <div class="btn-container">
+        <div class="selected">
+          <mwc-icon class="color-size">${this.selected.icon}</mwc-icon>
+
+          <span class="selected-text">${this.selected.type}</span>
+        </div>
+        <div>
+          ${this.vehicles.map(
+            (v) =>
+              html`<mwc-icon-button-toggle
+                @click=${(e) => this.handleIconBtnToggle(e)}
+                .on=${v.on}
+                .name=${v.type}
+                .type=${v.type}
+                onIcon="check"
+                offIcon=${v.icon}
+                title="${v.type}"
+              >
+              </mwc-icon-button-toggle> `
+          )}
+        </div>
+      </div>
+      <!-- <mwc-select
         @change=${(e) => this.handleSelect(e)}
         outlined
         label="Select vehicle type"
@@ -31,9 +84,9 @@ class VehicleSelector extends LitElement {
       >
         <mwc-list-item></mwc-list-item>
         ${this.vehicles.map(
-          (v) => html`<mwc-list-item .value=${v.type}>${v.type}</mwc-list-item>`
-        )}
-      </mwc-select>
+        (v) => html`<mwc-list-item .value=${v.type}>${v.type}</mwc-list-item>`
+      )}
+      </mwc-select> -->
     `;
   }
 
